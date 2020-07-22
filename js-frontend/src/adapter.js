436ab06bdd524106cb009a26ec21387c79905f20
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-22 03:30:26 -0400

    adapter.js retrieveItems function fetches all items

--- js-frontend/src/adapter.js
+++ js-frontend/src/adapter.js
@@ -2,9 +2,12 @@ class Adapter {
 
   constructor() {
     console.log('adapter.js was run')
-    const url = 'localhost:3000/items'
-    this.adapter = url
+    const url = 'http://localhost:3000/items'
+    this.baseURL = url
   }
 
-
+  retrieveItems(){
+    return fetch(this.baseURL)
+      .then(res => res.json)
+  }
 }
