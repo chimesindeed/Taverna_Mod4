d2fd3d0719a0ec15361ffe59b2f1b5491d9b8765
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-22 03:35:52 -0400

    app.js getItemsToDOM calls Adapter Class's 'retrieveItems()'

--- js-frontend/src/app.js
+++ js-frontend/src/app.js
@@ -2,5 +2,13 @@ class App {
   constructor(){
   console.log('app.js was run')
     this.adapter = new Adapter
+    this.getItemsToDOM()
+  }
+
+
+  getItemsToDOM(){
+    this.adapter.retrieveItems()
+      .then(items=> console.log(items))
+
   }
 }
