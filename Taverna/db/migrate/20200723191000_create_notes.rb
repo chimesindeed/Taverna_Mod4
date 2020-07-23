404e9ff01654f8d90b470b9c902a7e3fa400b219
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-23 19:51:28 -0400

    added notes table note has 'body' and 'item_id'

--- Taverna/db/migrate/20200723191000_create_notes.rb
+++ Taverna/db/migrate/20200723191000_create_notes.rb
@@ -1,7 +1,8 @@
 class CreateNotes < ActiveRecord::Migration[6.0]
   def change
     create_table :notes do |t|
-
+      t.string :body
+      t.integer :item_id
       t.timestamps
     end
   end
--- Taverna/db/schema.rb
+++ Taverna/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_22_020525) do
+ActiveRecord::Schema.define(version: 2020_07_23_191000) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.string "body"
+    t.integer "item_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
