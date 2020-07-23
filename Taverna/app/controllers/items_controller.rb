095e492a607fb652bf2c79a8b34665beca940bf8
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-23 13:38:21 -0400

    items_controller.rb added create action and strong params

--- Taverna/app/controllers/items_controller.rb
+++ Taverna/app/controllers/items_controller.rb
@@ -4,5 +4,17 @@ class ItemsController < ApplicationController
 
     render json: items
   end
-  
+
+  def create
+    binding.pry
+    item = Item.create(item_params)
+
+    render json: item
+end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name)
+  end
 end
