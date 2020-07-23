c2e1eb6ca316419584d37d2990177b0c9b877239
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-23 06:09:21 -0400

    added allItems array to contain retrieved items

--- js-frontend/src/app.js
+++ js-frontend/src/app.js
@@ -1,14 +1,25 @@
 class App {
   constructor(){
   console.log('app.js was run')
+    this.allItems = []
     this.adapter = new Adapter
-    this.getItemsToDOM()
+    this.getItems()
+    this.itemsToDOM()
   }
 
+  itemsToDOM(){
+   const ul = document.getElementById('ul-items-holder')
+   const li = document.createElement('li')
+  }
 
-  getItemsToDOM(){
-    this.adapter.retrieveItems()
-      .then(items=> console.log(items))
+  getItems(){
+    this.adapter
+      .getItems()
+      .then(items => {
+        items.forEach(item => this.allItems.push(new Item(item)))
+      .then(() => {
+        this.render()
+      })
 
   }
 }
