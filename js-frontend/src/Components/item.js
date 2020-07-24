2d48547c86ddef6385f079e992b6995c5e548275
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-24 03:51:32 -0400

    Item Class creates new Note object for each note and pushes onto array

--- js-frontend/src/Components/item.js
+++ js-frontend/src/Components/item.js
@@ -1,9 +1,17 @@
 class Item {
   constructor(itemJSON) {
+    this.notesArr = []
     this.id = itemJSON.id
     this.name = itemJSON.name
+    this.notes = itemJSON.notes
+    this.notes.forEach(note =>{
+      new Note(note)
+     this.notesArr.push(new Note(note))
+     console.log(this.notesArr)
+    })
   }
-    renderLi() {
+    renderItemLi() {
       return `<li>${this.name}</li>`
     }
+
 }
