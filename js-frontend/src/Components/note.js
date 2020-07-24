6d53d8f40c965a5360949d2666947984d4c68759
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-24 03:52:10 -0400

    Note objects have item_id

--- js-frontend/src/Components/note.js
+++ js-frontend/src/Components/note.js
@@ -2,9 +2,7 @@ class Note {
   constructor(noteJSON) {
     this.id = noteJSON.id
     this.body = noteJSON.body
+    this.item_id = noteJSON.item_id
   }
-    
-  renderLi() {
-      return `<li>${this.name}</li>`
-    }
+
 }
