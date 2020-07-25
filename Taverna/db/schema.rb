ef022be36416262bda782ef1ab8304ff95a66532
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-25 12:46:26 -0400

    added price column to items table

--- Taverna/db/schema.rb
+++ Taverna/db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2020_07_23_191000) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
+    t.float "price"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
