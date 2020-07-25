f92a58352197748df2f2888a382bd90123952074
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-25 17:22:32 -0400

    dom-elements.js - Abstracted DOM isolation into separate class

---
+++ js-frontend/src/Components/dom-elements.js
@@ -0,0 +1,11 @@
+class DOMElements{
+  constructor(){
+    this.ul = document.getElementById('ul-items-holder')
+    this.li = document.createElement('li')
+    this.inputName = document.getElementById('form-input-name')
+    this.inputPrice =  document.getElementById('form-input-price')
+    this.form = document.getElementById('create-item-form')
+    
+  }
+  
+}
