d608da5527dfe464f8cca0ee51312248e9842a36
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-27 14:04:29 -0400

    isolated DOM deleteButton

--- js-frontend/src/Components/dom-elements.js
+++ js-frontend/src/Components/dom-elements.js
@@ -1,14 +1,15 @@
 class DOMElements{
   constructor(){
+    this.body = document.querySelector('body')
     this.ul = document.getElementById('ul-items-holder')
-
-    this.li = document.createElement('li')
 
     this.inputName = document.getElementById('form-input-name')
 
     this.inputPrice =  document.getElementById('form-input-price')
 
     this.form = document.getElementById('create-item-form')
+
+    this.deleteButton = document.getElementById('button-delete-item')
 
   }
 
