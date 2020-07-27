07282209cfb6ca67fe7f28569e723286fe61937a
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-27 14:10:12 -0400

    added data- attribute to items before render

--- js-frontend/src/Components/render.js
+++ js-frontend/src/Components/render.js
@@ -3,21 +3,18 @@ class Render {
   constructor() {
 
     this.allItems = []
-    this.itemNotes = []
+    this.allNotes = []
     this.DOM = new DOMElements
     }
-  renderItemLi() { return `<li>${this.item.name}</li>` }
-  renderNoteLi() { return `<li>${this.body}</li> `}
 
   renderItems() {
-    return this.DOM.ul.innerHTML = this.allItems.map(item => `<li>${item.name}, ${item.price}</li>`).join('')
+    this.DOM.ul.innerHTML = this.allItems.map(item =>
+      `<li
+        data-hello=${item.id}>
+        ${item.name},
+        ${item.price}
+      </li>`)
+    .join('')
   }
 
-  renderNotes() {
-    this.itemNote.forEach(note => {
-      if (note.item_id == item.id){
-        console.log(note.body)
-      }
-    })
-  }
 }
