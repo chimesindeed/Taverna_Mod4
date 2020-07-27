469f608107d473d906b3ba8496b3167c39a77530
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-27 14:12:13 -0400

    added delete function to Adapter class

--- js-frontend/src/Adapter/adapter.js
+++ js-frontend/src/Adapter/adapter.js
@@ -4,6 +4,7 @@ class Adapter {
     console.log('adapter.js was run')
     const url = 'http://localhost:3000/items'
     this.baseURL = url
+    this.delete_url = `http://localhost:3000/items/3`
   }
 
   async getItems(){
@@ -12,10 +13,18 @@ class Adapter {
     return data;
   }
 
+  getNotes(e) {
+    const li = e.target
+    let arr = []
+    this.render.allNotes.map(note => {
+      if (note.item_id == li.dataset.hello){
+        arr.push(note.body)}})
+    this.DOM.ul.innerHTML = arr
+   }
  createItem(name, price){
   const item = {
-  name: name,
-  price: price
+    name: name,
+    price: price
   }
   return fetch(this.baseURL,{
     method: 'POST',
@@ -24,5 +33,11 @@ class Adapter {
     },
     body: JSON.stringify({item})
     }).then(res => res.json())
+  }
+deleteItem(item){
+  return fetch(this.delete_url, {
+    method: 'DELETE'
+  })
+  .then(res => res.json());
   }
 }
--- js-frontend/src/Components/item.js
+++ js-frontend/src/Components/item.js
@@ -1,15 +1,9 @@
 class Item {
   constructor(itemJSON) {
-    this.notesArr = []
+    // this.notesArr = []
     this.id = itemJSON.id
     this.name = itemJSON.name
     this.price = itemJSON.price
-    this.notes = itemJSON.notes
-
-    this.notes.forEach(note =>{
-      console.log(this.notesArr);
-      this.notesArr.push(new Note(note));
-    })
+    this.notes = itemJSON.note
   }
-
 }
