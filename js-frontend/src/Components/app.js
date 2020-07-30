bf7d2a5ccaef2c48a48c2385057f28c04009644e
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-30 15:53:57 -0400

    app.js - moved dom isolation into app.js constructor

--- js-frontend/src/Components/app.js
+++ js-frontend/src/Components/app.js
@@ -1,38 +1,42 @@
 class App {
 
   constructor() {
-  console.log('app.js was run')
+    this.form = document.getElementById('create-item-form')
+    this.ul = document.getElementById('ul-items-holder')
+    this.inputName = document.getElementById('form-input-name')
+    this.inputPrice =  document.getElementById('form-input-price')
+    
     this.adapter = new Adapter
     this.DOM = new DOMElements
     this.render = new Render
-    this.DOMListenersAndBindings()
     this.getItems()
+    this.DOMListenersAndBindings()
   }
 
   DOMListenersAndBindings() {
-    this.DOM.form.addEventListener('submit', this.createItem.bind(this))
-    this.DOM.ul.addEventListener('dblclick', this.adapter.getNotes.bind(this))
-    this.DOM.ul.addEventListener('click', this.deleteItem.bind(this))
+    this.form.addEventListener('submit', this.createItem.bind(this))
+    this.ul.addEventListener('dblclick', this.deleteItem.bind(this))
   }
   
   deleteItem(e){
   e.preventDefault
   const li = e.target
-    this.adapter.deleteData(li.dataset.hello, this.adapter.baseURL)
-    .then(data => console.log(data))
-      //this.render.allItems.forEach(item => {
-      //if (item.id==data){delete this.render.allItems[item]}}))
+    this.adapter.deleteData(li.dataset.item_id, this.adapter.baseURL)
+    .then(data => console.log(data));
+    this.render.allItems = []
+    this.ul.innerHTML=""
+    this.getItems()
  }
   
   createItem(e){
     e.preventDefault()
-    const name = this.DOM.inputName.value
-    const price = this.DOM.inputPrice.value
+    const name = this.inputName.value
+    const price = this.inputPrice.value
 
     this.adapter.createItem(name, price).then(item => {
       this.render.allItems.push(new Item(item))
-      this.DOM.inputName.value = ''
-      this.DOM.inputPrice.value = ''
+      this.inputName.value = ''
+      this.inputPrice.value = ''
       this.render.renderItems()
     })
   }
