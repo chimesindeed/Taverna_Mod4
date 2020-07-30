dba537748960f0b4561ddff76758c3ae2329a03b
Dimitri Gedda <gratitudeadore@fastmail.com>
2020-07-30 15:56:28 -0400

    dynamically assigned 'Notes' Buttons to all items.  Added listener with 'this' binding

--- js-frontend/src/Components/render.js
+++ js-frontend/src/Components/render.js
@@ -4,17 +4,32 @@ class Render {
 
     this.allItems = []
     this.allNotes = []
-    this.DOM = new DOMElements
+    this.ul = document.getElementById('ul-items-holder')
+    // this.DOM = new DOMElements
     }
 
   renderItems() {
-    this.DOM.ul.innerHTML = this.allItems.map(item =>
-      `<li
-        data-hello=${item.id}>
-        ${item.name},
-        ${item.price}
-      </li>`)
-    .join('')
+    this.ul.innerHTML = this.allItems.map(item =>
+      `<div id='item-holder'>
+         <li data-item_id=${item.id}>${item.name} - $${item.price}</li>
+       </div>
+       <div id= 'open-notes'>
+         <button id=${item.id} class="note-buttons">Notes</button>
+       </div>`).join('');
+    
+    this.noteButtons=document.getElementsByClassName('note-buttons');
+    let arr = Array.from(this.noteButtons).forEach(button =>
+      button.addEventListener('click', this.renderNotes.bind(this)))
+              
+  }
+  renderNotes(e) {
+    const button = e.target
+    let arr = []
+    this.allNotes.map(note => {
+        if (note.item_id == button.id){
+          arr.push(note.body)}
+        });
+    this.ul.innerHTML = arr
   }
 
 }
